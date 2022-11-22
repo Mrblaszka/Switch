df1d1022298560c18b64a5257e7beeaa091379d0
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-11-22 10:35:57 +0100

    Sortowanie

--- app/src/main/java/com/example/aswitch/MainActivity.kt
+++ app/src/main/java/com/example/aswitch/MainActivity.kt
@@ -15,8 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun sortowanie(arr:IntArray):IntArray{
+            var swap = true
+            while(swap){
+                swap = false
+                for(i in 0 until arr.size-1){
+                    if(arr[i] > arr[i+1]){
+                        val temp = arr[i]
+                        arr[i] = arr[i+1]
+                        arr[i + 1] = temp
 
+                        swap = true
+                    }
+                }
+            }
+            return arr
+        }
 
+        //Liczby
         var tx1 = findViewById<TextView>(R.id.nb0)
         var tx2 = findViewById<TextView>(R.id.nb1)
         var tx3 = findViewById<TextView>(R.id.nb2)
@@ -27,9 +43,23 @@ class MainActivity : AppCompatActivity() {
         var tx8 = findViewById<TextView>(R.id.nb7)
         var tx9 = findViewById<TextView>(R.id.nb8)
 
+        //Przyciski
+        val bt1 = findViewById<Switch>(R.id.sw1)
+        val bt2 = findViewById<ToggleButton>(R.id.tg1)
+        val bt3 = findViewById<ToggleButton>(R.id.tg2)
+        val bt4 = findViewById<ToggleButton>(R.id.tg3)
+        val bt5 = findViewById<Switch>(R.id.sw4)
+        val bt6 = findViewById<ToggleButton>(R.id.tg5)
+        val bt7 = findViewById<ToggleButton>(R.id.tg6)
+        val bt8 = findViewById<ToggleButton>(R.id.tg7)
+        val bt9 = findViewById<ToggleButton>(R.id.tg8)
+
+        //Domyślne ustawienie
+
+        //Funkcje
         val sort = findViewById<Button>(R.id.sort)
         val random = findViewById<Button>(R.id.random)
-        val sort_table = findViewById<TextView>(R.id.)
+        val sort_table = findViewById<TextView>(R.id.textarea)
 
         random.setOnClickListener {
             tx1.text = Random.nextInt(0,9).toString()
@@ -44,20 +74,51 @@ class MainActivity : AppCompatActivity() {
         }
 
         sort.setOnClickListener {
-            val bt1 = findViewById<Switch>(R.id.sw1)
-            val bt2 = findViewById<ToggleButton>(R.id.tg1)
-            val bt3 = findViewById<ToggleButton>(R.id.tg2)
-            val bt4 = findViewById<ToggleButton>(R.id.tg3)
-            val bt5 = findViewById<Switch>(R.id.sw4)
-            val bt6 = findViewById<ToggleButton>(R.id.tg5)
-            val bt7 = findViewById<ToggleButton>(R.id.tg6)
-            val bt8 = findViewById<ToggleButton>(R.id.tg7)
-            val bt9 = findViewById<ToggleButton>(R.id.tg8)
 
-            if(bt1.isActivated){
 
+            if(bt1.isSaveEnabled){
+                sort_table.append(tx1.text)
+                sort_table.append(",")
+            }
+            if(bt2.text=="ON"){
+                sort_table.append(tx2.text)
+                sort_table.append(",")
+            }
+            if(bt2.text=="ON"){
+                sort_table.append(tx3.text)
+                sort_table.append(",")
+            }
+            if(bt4.text=="ON"){
+                sort_table.append(tx4.text)
+                sort_table.append(",")
+            }
+            if(bt5.isSaveEnabled){
+                sort_table.append(tx5.text)
+                sort_table.append(",")
+            }
+            if(bt6.text=="ON"){
+                sort_table.append(tx6.text)
+                sort_table.append(",")
+            }
+            if(bt7.text=="ON"){
+                sort_table.append(tx7.text)
+                sort_table.append(",")
+            }
+            if(bt8.text=="ON"){
+                sort_table.append(tx8.text)
+                sort_table.append(",")
+            }
+            if(bt9.text=="ON"){
+                sort_table.append(tx9.text)
+                sort_table.append(",")
             }
 
+        }
+
+        //Sortowanie
+        sort.setOnClickListener {
+            val lista = sortowanie(intArrayOf(sort_table.text.toString().toInt()))
+            sort_table.text = lista.toString()
         }
 
     }
